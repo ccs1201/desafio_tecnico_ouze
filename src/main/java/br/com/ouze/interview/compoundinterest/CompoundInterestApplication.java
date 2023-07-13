4a6d31faab19c54e750ecc3d83f864db1277a4b5
andersonma <anderson.mata@calcard.com.br>
2023-07-13 17:23:56 -0300

    initial commit

---
+++ src/main/java/br/com/ouze/interview/compoundinterest/CompoundInterestApplication.java
@@ -0,0 +1,13 @@
+package br.com.ouze.interview.compoundinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoundInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompoundInterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ouze/interview/compoundinterest/CompoundInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ouze.interview.compoundinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompoundInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
